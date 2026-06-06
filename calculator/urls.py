5bc32f0baea4921c37f0e3c6d1fb0c36e4f65086
Linus-Tabari <leetamparega@gmail.com>
2026-06-06 21:50:59 +0000

    docs

--- calculator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('simple_calcus.urls'))
-]
--- simple_calcus/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimpleCalcusConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'simple_calcus'
